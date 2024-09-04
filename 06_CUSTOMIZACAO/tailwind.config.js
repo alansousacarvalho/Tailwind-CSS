181a354bb1380eba19c89acdf9a39dad8f0fc462
Alan de Sousa Carvalho <88456324+alansousacarvalho@users.noreply.github.com>
2024-09-04 16:32:31 -0300

    Conclusão da seção de customização

--- 06_CUSTOMIZACAO/tailwind.config.js
+++ 06_CUSTOMIZACAO/tailwind.config.js
@@ -1,9 +1,31 @@
+const { colors } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    // Alteração de COLORS
+    // colors: {
+    //   black: '#010101'
+    // },
+    // Inclusao de BREAKPOINT e MAX-WIDTH personalizado
+    // screens: {
+    // 'tablet': '640px',
+    // 'cel': {'max': '400px'}
+    // },
+    extend: {
+      colors: {
+        "new-blue": "#243C5A",
+        gray: {
+          ...colors.gray,
+          900: "#999",
+        },
+      }
+      // Inclusão de novo SPACING
+      // spacing: {
+      //   '50': '20rem'
+      // }
+    },
   },
   plugins: [],
-}
-
+};
